27ab05d85a62e9655d8ed3bf2c40312df8d8260e
v-jacx <jvelasquez6798@gmail.com>
2022-07-20 09:25:08 -0400

    required deliverables completed

--- answers.js
+++ answers.js
@@ -1,31 +1,41 @@
 // create 5 burgers (at least 3 should be beef)
+db.burgers.insertMany([
+{burger: 'beef', cheese: true, toppings:['tomato', 'onions', 'ketchup', 'mayo', 'lettuce']},
+{burger: 'beef', cheese: false, toppings:['tomato', 'onions', 'ketchup', 'mayo', 'lettuce']},
+{burger: 'beef', cheese: true, toppings:['bacon', 'onion straws', 'bbq sauce']},
+{burger: 'chicken', cheese: true, toppings:['buffalo sauce', 'ranch']},
+{burger: 'chicken', cheese: true, toppings:['lettuce', 'tomato', 'bacon']},
+])
 
 // find all the burgers
+db.burgers.find({})
 
 // show just the meat of each burger
+db.burgers.find({},{burger:1})
 
 // show just the toppings of each burger
-
+db.burgers.find({},{toppings:1})
 // show everything but the cheese
-
+db.burgers.find({},{cheese: 0})
 // find all the burgers with beef
-
+db.burgers.find({burger: 'beef'})
 // find all the burgers that are not beef
-
+db.burgers.find({burger: {$ne: 'beef'}})
 // find the first burger with cheese
-
+db.burgers.findOne({cheese: true})
 // find one and update the first burger with cheese to have a property of 'double cheese'
-
+db.burgers.updateOne({cheese: true}, {$set: {doubleCheese: true}, $currentDate: {lastModified: true}})
 // find the burger you updated to have double cheese
-
+db.burgers.find({doubleCheese: true})
 // find and update all the beef burgers to be 'veggie'
-
+db.burgers.updateMany({burger: 'beef'},{$set: {burger: 'veggie'}, $currentDate:{lastModified: true}})
 // delete one of your veggie burgers
 // WRONG - dELETES ALL : db.burger.remove({meat: 'veggie'})
-
+db.burgers.deleteOne({ burger: 'veggie'})
 // drop the collection
 //Expected Output
 //true
+db.burgers.drop()
 
 // drop the database
 //Expected Output
@@ -33,7 +43,7 @@
 //   "dropped": "burgers",
 //   "ok": 1
 // }
-
+db.dropDatabase()
 //
 // Bonus
 //recreate your burgers database and your burger collection
